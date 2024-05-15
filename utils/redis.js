f81750f936a3a54d76f0d7e431438c6df5167e87
Abdullah Bedwey <bedweyy@gmail.com>
2024-05-15 14:20:19 +0300

    chore: Refactor RedisClient class to improve connection handling

--- utils/redis.js
+++ utils/redis.js
@@ -1,16 +1,22 @@
 import redis from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
     constructor() {
         this.client = redis.createClient();
         this.isClientConnected = true;
         this.client.on('error', (error) => {
+            this.isClientConnected = false;
             console.error(error);
         });
 
         this.client.on('connect', () => {
             this.isClientConnected = true;
         });
+
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
     }
 
     isAlive() {
