8457905ddd497005df44d1aed4d89c0692540e5a
Abdullah Bedwey <bedweyy@gmail.com>
2024-05-15 15:33:21 +0300

    chore: Refactor UsersController to handle missing email and password gracefully

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,7 +3,8 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
 
     if (!email) {
       res.status(400).send({ error: 'Missing email' });
