a937c11fadef0c0621788c92fd0a36f7385a1e34
Abdullah Bedwey <bedweyy@gmail.com>
2024-05-15 14:56:03 +0300

    chore: Refactor RedisClient class

--- utils/redis.js
+++ utils/redis.js
@@ -2,39 +2,39 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.isClientConnected = true;
-        this.client.on('error', (error) => {
-            this.isClientConnected = false;
-            console.error(error);
-        });
+  constructor() {
+    this.client = redis.createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (error) => {
+      this.isClientConnected = false;
+      console.error(error);
+    });
 
-        this.client.on('connect', () => {
-            this.isClientConnected = true;
-        });
+    this.client.on('connect', () => {
+      this.isClientConnected = true;
+    });
 
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.isClientConnected;
-    }
+  isAlive() {
+    return this.isClientConnected;
+  }
 
-    async get(key) {
-        const value = await this.getAsync(key);
-        return value;
-    }
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        await this.setAsync(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        await this.delAsync(key);
-    }
+  async del(key) {
+    await this.delAsync(key);
+  }
 }
 
 const redisClient = new RedisClient();
